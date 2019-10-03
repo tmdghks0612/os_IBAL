65c2d5090c4e76dcdc93141b4e9c06f26bb9b20a
sinunu <sew9869@gmail.com>
2019-10-04 00:41:11 +0900

    10.03 last commit

--- p02/main.c
+++ p02/main.c
@@ -1,12 +1,12 @@
-#include "a.h"
+#include "main.h"
 
 int main() {
 	char user_command[300]; // user command
 	char *comtok[10]; // tockenized user commands
 	int list_num = 0, hash_num = 0, bit_num = 0;
-	ListList listarr[10];
-	HashList hasharr[10];
-	BitList bitlist[10];
+	ListList listarr[MAX_ARRAY_NUM];
+	HashList hasharr[MAX_ARRAY_NUM];
+	BitList bitlist[MAX_ARRAY_NUM];
 
 	while (1) {
 		printf("Input : ");
@@ -49,11 +49,9 @@ int main() {
 			if (comtok[2])
 				puts("Input is wrong! Too many input value");
 			else {
-				int i;
-				for (i = 0; i < list_num; ++i)
-					if (!strcmp(listarr[i].name, comtok[1]))
-						break; 
-				if (i != list_num) { // dumpadata list
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+
+				if (i != MAX_ARRAY_NUM) { // dumpadata list
 					IntList *temp;
 					for (struct list_elem* e = list_begin (&(listarr[i].list)); e != list_end (&(listarr[i].list)); e = list_next (e)) {
 						temp = list_entry (e, IntList, elem);
@@ -67,26 +65,29 @@ int main() {
 			}
 		}
 		else if (!strcmp(comtok[0], "delete")) {
-			int i;
-				for (i = 0; i < list_num; ++i)
-					if (!strcmp(listarr[i].name, comtok[1]))
-						break; 
-				if (i != list_num) { // delete list
-					IntList* temp;
-					struct list_elem *e;
-					while (!list_empty(&(listarr[i].list))) {
-						e = list_begin(&(listarr[i].list));
-						list_remove(e);
-						temp = list_entry(e, IntList, elem);
-						free(temp);
-					}
-					for (int j = i; j < list_num-1; ++j)
-						listarr[j] = listarr[j+1];
-					list_num--;
-				}
-				else {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
 
+			if (comtok[2])
+				puts("Input is wrong! Too many input value");
+
+			int i = FindArray((void*)listarr, comtok[1], LIST);
+			if (i != MAX_ARRAY_NUM) { // delete list
+				IntList* temp;
+				struct list_elem *e;
+				while (!list_empty(&(listarr[i].list))) {
+					e = list_begin(&(listarr[i].list));
+					list_remove(e);
+					temp = list_entry(e, IntList, elem);
+					free(temp);
 				}
+				for (int j = i; j < list_num-1; ++j)
+					listarr[j] = listarr[j+1];
+				list_num--;
+			}
+			else {
+
+			}
 		}
 		else if (!strcmp(comtok[0], "list_insert")) {
 			comtok[1] = strtok(NULL, " ");
@@ -97,12 +98,9 @@ int main() {
 			if (comtok[4])
 				puts("Input is wrong! Too many input value");
 			else {
-				int i;
-				for (i = 0 ; i < list_num; ++i)
-					if (!strcmp(listarr[i].name, comtok[1]))
-						break;
+				int i = FindArray((void*)listarr, comtok[1], LIST);
 				
-				if (i == list_num) // 해당 이름의 list가 없을 경우
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
 					puts("There is not list of name you enter!");
 				else {
 					struct list_elem* e = list_begin(&(listarr[i].list));
@@ -119,10 +117,134 @@ int main() {
 				}
 			}
 		}
+		else if (!strcmp(comtok[0], "list_push_front")) {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+			comtok[3] = strtok(NULL, " ");
+
+			if (comtok[3])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					IntList *e = (IntList*)malloc(sizeof(IntList));
+					e->val = atoi(comtok[2]);
+
+					list_push_front(&(listarr[i].list), &(e->elem));
+					}
+			}
+		}
 		else if (!strcmp(comtok[0], "list_push_back")) {
-				
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+			comtok[3] = strtok(NULL, " ");
+
+			if (comtok[3])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					IntList *e = (IntList*)malloc(sizeof(IntList));
+					e->val = atoi(comtok[2]);
+
+					list_push_back(&(listarr[i].list), &(e->elem));
+				}
+			}
+		}
+		else if (!strcmp(comtok[0], "list_front")) {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+
+			if (comtok[2])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					struct list_elem *e = list_front(&(listarr[i].list));
+					printf("%d\n", list_entry(e, IntList, elem)->val);
+				}
+			}
+		}
+		else if (!strcmp(comtok[0], "list_back")) {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+
+			if (comtok[2])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					struct list_elem *e = list_back(&(listarr[i].list));
+					printf("%d\n", list_entry(e, IntList, elem)->val);
+				}
+			}
+		}
+		else if (!strcmp(comtok[0], "list_pop_front")) {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+
+			if (comtok[2])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					struct list_elem *e = list_pop_front(&(listarr[i].list));
+					free(list_entry(e, IntList, elem));
+				}
+			}
+		}
+		else if (!strcmp(comtok[0], "list_pop_back")) {
+			comtok[1] = strtok(NULL, " ");
+			comtok[2] = strtok(NULL, " ");
+
+			if (comtok[2])
+				puts("There are too many parameter!");
+			else {
+				int i = FindArray((void*)listarr, comtok[1], LIST);
+				if (i == MAX_ARRAY_NUM) // 해당 이름의 list가 없을 경우
+					puts("There is not list of name you enter!");
+				else {
+					struct list_elem *e = list_pop_back(&(listarr[i].list));
+					free(list_entry(e, IntList, elem));
+				}
+			}
 		}
 	}
 
 	return 0;
+}
+
+int FindArray(void* arr, char *name, int data) {
+	int i;
+	if (data == LIST) {
+		for (i = 0; i < 10; ++i)
+			if (!strcmp(((ListList*)arr)[i].name, name))
+				break; 
+	}
+	else if (data == BITMAP) {
+		for (i = 0; i < 10; ++i)
+			if (!strcmp(((BitList*)arr)[i].name, name))
+				break; 
+	}
+	else if (data == HASH) {
+		for (i = 0; i < 10; ++i)
+			if (!strcmp(((HashList*)arr)[i].name, name))
+				break; 
+	}
+	else {
+		puts("Data Parameter Value error!");
+		return -1;
+	}
+
+	return i;
 }
--- p02/main.h
+++ p02/main.h
@@ -7,6 +7,12 @@
 #include "round.h"
 #include "limits.h"
 
+#define MAX_ARRAY_NUM 10
+
+enum DataType {
+    LIST, BITMAP, HASH
+};
+
 typedef struct tagListList {
     char name[20];
     struct list list;
@@ -23,3 +29,5 @@ typedef struct tagIntList {
     struct list_elem elem;
     int val;
 } IntList;
+
+int FindArray(void* arr, char *name, int data); // Data의 이름을 받아 array의 index를 반환
